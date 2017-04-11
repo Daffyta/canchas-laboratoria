f40494154a6cf920c6705af9290e865077d652bc
Daffyta <daffy.dine9@gmail.com>
2017-04-11 11:57:44 -0500

    Agregando canchas

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var botonBasket =document.getElementById('basket');
+var botonFut= document.getElementById('fut');
+var botonTennis = document.getElementById('tennis');
+
+botonFut.addEventListener("click",mostrarFut);
+botonBasket.addEventListener("click", mostrarBasket);
+botonTennis.addEventListener("click",mostrarTennis);
+
+function mostrarBasket(){
+ document.getElementById('basketball').style.display ="block";
+ document.getElementById('futball').style.display ="none";
+}
+
+function mostrarFut(){
+  document.getElementById('futball').style.display ="block";
+  document.getElementById('basketball').style.display ="none";
+}
+
+function mostrarTennis(){
+  document.getElementById('tennis').style.display ="block";
+ document.getElementById('basketball').style.display ="none";
+ document.getElementById('futball').style.display ="none";
+}
