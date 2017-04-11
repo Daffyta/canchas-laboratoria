2327fa2aa53f4e22d9cd0a96abe1229386b74b64
Daffyta <daffy.dine9@gmail.com>
2017-04-11 12:09:11 -0500

    Correcciones

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var botonBasket =document.getElementById('basket');
 var botonFut= document.getElementById('fut');
-var botonTennis = document.getElementById('tennis');
+var botonTennis = document.getElementById('ten');
 
 botonFut.addEventListener("click",mostrarFut);
 botonBasket.addEventListener("click", mostrarBasket);
@@ -9,15 +9,16 @@ botonTennis.addEventListener("click",mostrarTennis);
 function mostrarBasket(){
  document.getElementById('basketball').style.display ="block";
  document.getElementById('futball').style.display ="none";
+ document.getElementById('ten').style.display ="none";
 }
 
 function mostrarFut(){
   document.getElementById('futball').style.display ="block";
   document.getElementById('basketball').style.display ="none";
+  document.getElementById('ten').style.display ="none";
 }
-
 function mostrarTennis(){
-  document.getElementById('tennis').style.display ="block";
- document.getElementById('basketball').style.display ="none";
- document.getElementById('futball').style.display ="none";
+  document.getElementById('ten').style.display ="block";
+  document.getElementById('basketball').style.display ="none";
+  document.getElementById('futball').style.display ="none";
 }
